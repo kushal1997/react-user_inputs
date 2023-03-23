ce01a21274db793b3b3ac05a1d91edffdec7681a
kushal1997 <kushalrao103@gmail.com>
2023-03-23 11:27:07 +0530

    controlled inputs

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,14 +3,19 @@ import { useState } from "react";
 
 export const AddTask = () => {
     const [taskValue,setTaskValue]=useState("");
+
     const handleChange=(event) => {
         setTaskValue(event.target.value);
+    }
+    const handleReset=()=>{
+        setTaskValue("");
     }
   return (
     <section className="addtask">
         <form>
-            <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
+            <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" value={taskValue} />
             <button type="submit">Add Task</button>
+            <span onClick={handleReset} className="reset">Reset</span>
         </form>
         <p>{taskValue}</p>
     </section>  
