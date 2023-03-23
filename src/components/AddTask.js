5c518b8a1589995bfa4d4150ceb5ce01ffa6102b
kushal1997 <kushalrao103@gmail.com>
2023-03-23 12:04:17 +0530

    onSubmit event

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,19 +3,35 @@ import { useState } from "react";
 
 export const AddTask = () => {
     const [taskValue,setTaskValue]=useState("");
-
+    const [progress,setProgress]=useState(false);
     const handleChange=(event) => {
         setTaskValue(event.target.value);
     }
     const handleReset=()=>{
         setTaskValue("");
+        setProgress(false);
+    }
+    const handleSubmit=(event) =>{
+        event.preventDefault();
+        const task={
+            id: Math.floor(Math.random()*1000000),
+            name: taskValue,
+            completed: Boolean(progress)
+        }
+        console.log(task);
+        handleReset();
     }
   return (
     <section className="addtask">
-        <form>
+        <form onSubmit={handleSubmit}>
             <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" value={taskValue} />
-            <button type="submit">Add Task</button>
+            <select onChange={(event) =>{setProgress(event.target.value)}} value={progress}>
+                <option value={false}>Pending</option>
+                <option value={true}>Completed</option>
+            </select>
             <span onClick={handleReset} className="reset">Reset</span>
+            <button  type="submit">Add Task</button>
+            
         </form>
         <p>{taskValue}</p>
     </section>  
